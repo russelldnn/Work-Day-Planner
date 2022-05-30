5683492f0b700ba0303d42f92328576e16b9bdd3
russelldnn <russelldnn@gmail.com>
2022-05-30 03:39:43 -0400

    added most of the comment text

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ setInterval(function(){
     $("#currentDay").text(moment().format("MMMM Do, YYYY, h:mm:ss a"));
 
 },1000);
-
+//event click listener for the save button on each row to save its associate rows user input
 $(".saveBtn").on("click", function(){
     let goals = $(this).siblings(".description").val();
     let desc = $(this).parent().attr("id");
@@ -11,6 +11,7 @@ $(".saveBtn").on("click", function(){
     localStorage.setItem(desc, goals);
 })
 
+//gets each rows stored user input by having the unique call name be the ID as set up in the variable desc
 $("#8 .description").val(localStorage.getItem("8"));
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
@@ -22,6 +23,7 @@ $("#15 .description").val(localStorage.getItem("15"));
 $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
 
+//a .each loop to cycle through each row (got help with this) replaces having to write out a lot of repeated code, this parses the "hour" from the associated ID
 function timeTest(){
     let currentTime = moment().hour();
     console.log(currentTime);
@@ -29,7 +31,7 @@ function timeTest(){
     $(".time-block").each(function () {
         let thisHour = parseInt($(this).attr("id"));
         console.log(thisHour);
-
+        //if else statements to compare the difference between the rows hour and the actual time and assigns classes to change the color dependantly
         if (thisHour === currentTime) {
             $(this).addClass ("present");
             $(this).removeClass ("past");
@@ -45,4 +47,5 @@ function timeTest(){
         }
     })
 }
+//function call
 timeTest();
