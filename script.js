90d366ba7b39333c4d70ac92d053ac6d2b0e7284
russelldnn <russelldnn@gmail.com>
2022-05-30 02:00:53 -0400

    save btn functionality to push to local storage

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ setInterval(function(){
     $("#currentDay").text(moment().format("MMMM Do, YYYY, h:mm:ss a"));
 
 },1000);
+
+$(".saveBtn").on("click", function(){
+    let goals = $(this).siblings(".description").val();
+    localStorage.setItem(desc, goals);
+})
