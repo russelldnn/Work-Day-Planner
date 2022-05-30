83bb653400de5292bdf9ce315154b117e6f200b4
russelldnn <russelldnn@gmail.com>
2022-05-30 03:31:04 -0400

    finished the color loop and figured out why my css wasnt loading

--- script.js
+++ script.js
@@ -11,13 +11,38 @@ $(".saveBtn").on("click", function(){
     localStorage.setItem(desc, goals);
 })
 
-$("#8am .description").val(localStorage.getItem("8am"));
-$("#9am .description").val(localStorage.getItem("9am"));
-$("#10am .description").val(localStorage.getItem("10am"));
-$("#11am .description").val(localStorage.getItem("11am"));
-$("#12pm .description").val(localStorage.getItem("12pm"));
-$("#1pm .description").val(localStorage.getItem("1pm"));
-$("#2pm .description").val(localStorage.getItem("2pm"));
-$("#3pm .description").val(localStorage.getItem("3pm"));
-$("#4pm .description").val(localStorage.getItem("4pm"));
-$("#5pm .description").val(localStorage.getItem("5pm"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+function timeTest(){
+    let currentTime = moment().hour();
+    console.log(currentTime);
+
+    $(".time-block").each(function () {
+        let thisHour = parseInt($(this).attr("id"));
+        console.log(thisHour);
+
+        if (thisHour === currentTime) {
+            $(this).addClass ("present");
+            $(this).removeClass ("past");
+            $(this).removeClass ("future");
+        } else if (thisHour < currentTime) {
+            $(this).addClass ("past");
+            $(this).removeClass ("present");
+            $(this).removeClass ("future");
+        } else {
+            $(this).addClass ("future");
+            $(this).removeClass ("past");
+            $(this).removeClass ("present")
+        }
+    })
+}
+timeTest();
