04bb0328e29310bff75e4606a26e19a4ba8bb9aa
russelldnn <russelldnn@gmail.com>
2022-05-30 02:14:58 -0400

    got the getitem working to return from local storage

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ setInterval(function(){
 
 $(".saveBtn").on("click", function(){
     let goals = $(this).siblings(".description").val();
+    let desc = $(this).parent().attr("id");
+    //received help in using the variable desc to select for each div block
     localStorage.setItem(desc, goals);
 })
+
+$("#8am .description").val(localStorage.getItem("8am"));
+$("#9am .description").val(localStorage.getItem("9am"));
+$("#10am .description").val(localStorage.getItem("10am"));
+$("#11am .description").val(localStorage.getItem("11am"));
+$("#12pm .description").val(localStorage.getItem("12pm"));
+$("#1pm .description").val(localStorage.getItem("1pm"));
+$("#2pm .description").val(localStorage.getItem("2pm"));
+$("#3pm .description").val(localStorage.getItem("3pm"));
+$("#4pm .description").val(localStorage.getItem("4pm"));
+$("#5pm .description").val(localStorage.getItem("5pm"));
